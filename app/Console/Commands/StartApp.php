7a04d4921e12b8f73f201d70b43ac44dba4d6f45
Fatma Ashraf <fatmaashraf12100@gmail.com>
2024-03-31 23:43:51 +0200

    add one command for run application

---
+++ app/Console/Commands/StartApp.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class StartApp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Start Application with one command';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+        $process = new Process(['composer', 'install']);
+        $process->run();
+        if (!$process->isSuccessful()) {
+            $this->error('Composer install failed: '.$process->getErrorOutput());
+            return;
+        }
+        $this->call('migrate');
+        $this->call('db:seed');
+        $this->info('Composer install completed successfully.');
+    
+    }
+}
