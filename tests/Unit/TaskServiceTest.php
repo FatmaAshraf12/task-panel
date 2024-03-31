75ef46ccbb0f72d5f3eef860808c660e3f53ee1e
Fatma Ashraf <fatmaashraf12100@gmail.com>
2024-04-01 01:08:21 +0200

    push unit tests

--- tests/Unit/TaskServiceTest.php
+++ tests/Unit/TaskServiceTest.php
@@ -51,5 +51,42 @@ class TaskServiceTest extends TestCase
         $response->assertRedirect('/');
     }
 
+    public function test_task_not_be_created()
+    {
+        $admin = User::factory()->create();
+        $adminRole = Role::where('name', 'admin')->first();
+        $admin->assignRole($adminRole);
+        $this->actingAs($admin);
+
+        $user = User::factory()->create();
+        $userRole = Role::where('name', 'user')->first();
+        $user->assignRole($userRole);
+
+
+          // Simulate a POST request to add task with unvalid data
+          $response = $this->post('/store', [
+            'user_id'=>$user->id,
+            'admin_id'=>$admin->id
+        ]);
+
+
+        // Assert the response status code
+        $response->assertRedirect('/create');
+    }
+
+    public function test_user_cannot_login()
+    {
+        $user = User::factory()->create();
+        $userRole = Role::where('name', 'user')->first();
+        $user->assignRole($userRole);
+        $this->actingAs($user);
+
+        // Simulate a get request to check user not admin login
+        $response = $this->get('/create');
+
+        // Assert the response status code
+        $response->assertRedirect('/login');
+    }
+
     
 }
